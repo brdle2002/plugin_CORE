801586c37a6229fe5c662a300752e969c54ff4a7
DEKTOP-BRAD\Brad <brad.c.hurst@gmail.com>
2025-03-23 12:24:13 -0400

    Remove the Manager stuff

--- Plugins/CORE_Minimal/Source/CORE_Minimal/Public/CORE/Libraries/CORE_GameManager_BPL.h
+++ Plugins/CORE_Minimal/Source/CORE_Minimal/Public/CORE/Libraries/CORE_GameManager_BPL.h
@@ -4,10 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/SubsystemBlueprintLibrary.h"
-#include "GameplayTagContainer.h"
 #include "CORE_GameManager_BPL.generated.h"
 
 class UCORE_GameManager;
+struct FGameplayTag;
 
 /**
  * A library of Blueprint functions for Blueprints to interface with the Game Manager subsystem.
@@ -17,8 +17,10 @@ class CORE_MINIMAL_API UCORE_GameManager_BPL : public USubsystemBlueprintLibrary
 {
 	GENERATED_BODY()
 
-public:
 
+
+/** OBJECT REFERENCE HANDLING **/
+public:
 	/* Register a reference with a specific Tag. References will ONLY be stored as UObjects. Can cast to requested class if neccessary.*/
 	UFUNCTION(BlueprintCallable, Category = "Game Manager|Reference Registry", meta = (DefaultToSelf = "Object"))
 		static void RegisterReference(FGameplayTag ID, UObject* Object);
@@ -27,6 +29,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Game Manager|Reference Registry")
 		static UObject* GetReference(FGameplayTag ID);
 
+private:
 		static UCORE_GameManager* GetGameManager();
 	
 };
